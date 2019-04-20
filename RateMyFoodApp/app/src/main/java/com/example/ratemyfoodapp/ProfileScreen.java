acba53b1c995573433ecbd736af33c88bf3b531b
kseveur3 <kseveur@hotmail.com>
2019-04-20 03:52:33 -0400

    Commit

--- RateMyFoodApp/app/src/main/java/com/example/ratemyfoodapp/ProfileScreen.java
+++ RateMyFoodApp/app/src/main/java/com/example/ratemyfoodapp/ProfileScreen.java
@@ -1,29 +1,128 @@
 package com.example.ratemyfoodapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ProfileScreen extends AppCompatActivity {
 
     private Button Settings;
+    private static final String TAG = "ProfileActivity";
+    private FirebaseDatabase mFireBaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mynewRef;
+    private String userID;
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
 
-        Settings = (Button) findViewById(R.id.btnSetting);
+        mAuth = FirebaseAuth.getInstance();
+        mFireBaseDatabase = FirebaseDatabase.getInstance();
+        mynewRef = mFireBaseDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        userID = user.getUid();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged : signed in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "OnAuthStateChanged: signed-out");
+                    toastMessage("Successfully signed out");
+                }
+            }
+        };
 
 
-       /* Settings.setOnClickListener(new View.OnClickListener() {
+        mynewRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    /*   Settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent hostIntent = new Intent(ProfileScreen.this, SettingScreen.class);
-                startActivity(hostIntent);
+                Intent ProfileIntent = new Intent(ProfileScreen.this, MainScreen.class);
+                startActivity(ProfileIntent);
             }
         });*/
     }
+
+    private void showData(DataSnapshot dataSnapshot) {
+        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+            Member memberInfo = new Member();
+            memberInfo.setFirst(ds.child(userID).getValue(Member.class).getFname());  // set the first name
+            memberInfo.setLast(ds.child(userID).getValue(Member.class).getLname());  // set the first name
+            memberInfo.setTheLogin(ds.child(userID).getValue(Member.class).getLoginId());  // set the first name
+            memberInfo.setPhoneNum(ds.child(userID).getValue(Member.class).getPhone());  // set the first name
+
+
+            ArrayList<String> array = new ArrayList<>();
+            array.add(memberInfo.getFname());
+            array.add(memberInfo.getLname());
+            array.add(memberInfo.getLoginId());
+            array.add(memberInfo.getPhone());
+            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, array);
+            mListView.setAdapter(adapter);
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener((mAuthListener));
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
+
+
