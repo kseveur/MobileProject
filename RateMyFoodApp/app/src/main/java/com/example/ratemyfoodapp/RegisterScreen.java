e1c8f9a9fe192649fec81d04559797ee853e06e5
kseveur <kseveur@hotmail.com>
2019-04-24 14:42:08 -0400

    Commit

--- RateMyFoodApp/app/src/main/java/com/example/ratemyfoodapp/RegisterScreen.java
+++ RateMyFoodApp/app/src/main/java/com/example/ratemyfoodapp/RegisterScreen.java
@@ -1,6 +1,8 @@
 package com.example.ratemyfoodapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,19 +10,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterScreen extends AppCompatActivity {
 
-    private EditText FirstName;
-    private EditText LastName;
-    private EditText Login;
+
+    private EditText Email;
     private EditText Password;
-    private EditText PhoneNumber;
     private Button Submit;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth fireBaseAuth;
     private String dbUserID;
 
     DatabaseReference databaseMembers;
@@ -31,12 +38,11 @@ public class RegisterScreen extends AppCompatActivity {
         setContentView(R.layout.activity_register_screen);
 
         databaseMembers = FirebaseDatabase.getInstance().getReference("members");
-        FirstName = (EditText) findViewById(R.id.etFirstName);
-        LastName = (EditText) findViewById(R.id.etLastName);
-        Login = (EditText) findViewById(R.id.etNewLogin);
+        fireBaseAuth = FirebaseAuth.getInstance();
+        Email = (EditText) findViewById(R.id.etEmail);
         Password = (EditText) findViewById(R.id.etNewPassword);
-        PhoneNumber = (EditText) findViewById(R.id.etPhone);
         Submit = (Button) findViewById(R.id.btnRegister);
+        progressDialog = new ProgressDialog(this);
 
         Submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +54,42 @@ public class RegisterScreen extends AppCompatActivity {
     }
 
     private void registerMember() {
+        String email = Email.getText().toString().trim();
+        String psswd = Password.getText().toString();
+
+
+
+        if((!TextUtils.isEmpty(email)) && !TextUtils.isEmpty(psswd)) {
+            progressDialog.setMessage(("Registering User......"));
+            progressDialog.show();
+
+            fireBaseAuth.createUserWithEmailAndPassword(email, psswd)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                toastMessage("New Member Added");
+                                Intent intent = new Intent(RegisterScreen.this, MainScreen.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+
+
+
+           /* String id = databaseMembers.push().getKey();
+            //Member member = new Member(Fname, Lname, LoginID, Phone);
+
+            databaseMembers.child(id).child("Login").setValue(email);
+            databaseMembers.child(id).child("Password").setValue(psswd);
+            databaseMembers.child(id).child("Fname").setValue(fname);
+            databaseMembers.child(id).child("Lnams").setValue(lname);
+            databaseMembers.child(id).child("Phone").setValue(phone);
+
+
+
+            Log.d("CREATION", Fname);
+=======
         String Fname = FirstName.getText().toString().trim();
         String Lname = LastName.getText().toString().trim();
         String LoginID = Login.getText().toString().trim();
@@ -56,16 +98,20 @@ public class RegisterScreen extends AppCompatActivity {
         if(!TextUtils.isEmpty(LoginID)) {
             String id = databaseMembers.push().getKey();
             Member member = new Member(Fname, Lname, LoginID, Phone);
+>>>>>>> 77a522bc99485c0ac3cf812b62b05671d6091de7
 
-            databaseMembers.child(id).setValue(member);
 
 
             Toast.makeText(this, "New Member Added", Toast.LENGTH_LONG).show();
-           // Intent intent = new Intent(RegisterScreen.this, LoginScreen.class);
-           // startActivity(intent);
+            Intent intent = new Intent(RegisterScreen.this, LoginScreen.class);
+            startActivity(intent); */
 
         } else {
             Toast.makeText(this, "You need to enter a login and password", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
